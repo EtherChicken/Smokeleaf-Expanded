a93d46e198d29f6f566099fb39c7e3e1b4a3f901
Ethan Van Horn <ethanvanhorn@gmail.com>
2022-11-18 10:09:20 -0600

    initial commit

---
+++ Source/Smokeleaf Expanded/Smokeleaf Expanded/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace ETH_Smokeleaf_Expanded
+{
+    [StaticConstructorOnStartup]
+    public class Edibles : DefModExtension
+    {
+
+    }
+    
+}
---
+++ Source/Smokeleaf Expanded/Smokeleaf Expanded/Hello_World.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Smokeleaf_Expanded
+{
+    [StaticConstructorOnStartup]
+    public static class MyMod
+    {
+        static MyMod()
+        {
+            Log.Message("Hello World!");
+        }
+    }
+}   

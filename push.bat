0d8f2ed898f54d379d3c2e521503a96ce7bd8366
Ethan Van Horn <ethanvanhorn@gmail.com>
2022-12-31 15:51:52 -0600

    Finished up the mod

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 
 :: Copies to the actual RimWorld folder with my mods
-xcopy /e /v /i "C:\Users\vanho\Desktop\Rimworld Things\My Rimworld Mods\Smokeleaf Expanded\" "C:\Users\vanho\Desktop\Rimworld Things\RimWorld\Mods\Smokeleaf Expanded\"
+xcopy /e /v /i "C:\Users\vanho\Desktop\Rimworld Things\My Rimworld Mods\Smokeleaf Expanded\" "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods"
 
 :: Copies to my RimWorld_For_Modding folder, for isolated testing with my created mods
 xcopy /e /v /i "C:\Users\vanho\Desktop\Rimworld Things\My Rimworld Mods\Smokeleaf Expanded\" "C:\Users\vanho\Desktop\Rimworld Things\RimWorld_For_Modding\Mods\Smokeleaf Expanded\"

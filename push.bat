c0c383d86f488fc286646dffea24352102bb8e73
Ethan Van Horn <ethanvanhorn@gmail.com>
2023-01-11 15:04:16 -0600

    Optimized Defs and tweaked values

--- push.bat
+++
@@ -1,9 +0,0 @@
-
-:: Copies to the actual RimWorld folder with my mods
-xcopy /e /v /i "C:\Users\vanho\Desktop\Rimworld Things\My Rimworld Mods\Smokeleaf Expanded\" "C:\Users\vanho\Desktop\Rimworld Things\RimWorld\Mods\Smokeleaf Expanded\"
-
-:: Copies to my RimWorld_For_Modding folder, for isolated testing with my created mods
-xcopy /e /v /i "C:\Users\vanho\Desktop\Rimworld Things\My Rimworld Mods\Smokeleaf Expanded\" "C:\Users\vanho\Desktop\Rimworld Things\RimWorld_For_Modding\Mods\Smokeleaf Expanded\"
-
-
-pause
